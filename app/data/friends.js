1c2cfd0ab0a1023091fd103fa8c232c7008537d7
rayjshin <rayjshin@gmail.com>
2017-08-07 22:05:58 -0400

    routes, some survey ...

---
+++ app/data/friends.js
@@ -0,0 +1,469 @@
+var friends = [
+  {
+    name: "James Bond",
+    photo: "",
+    scores: [
+      "5",
+      "1",
+      "4",
+      "4",
+      "5",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1"
+
+    ]
+},
+  {
+  name: "Ahmed",
+  photo: "https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
+  scores: [
+    "5",
+    "1",
+    "4",
+    "4",
+    "5",
+    "1",
+    "2",
+    "5",
+    "4",
+    "1"
+  ]
+},
+  {
+  name: "Jacob Deming",
+  photo: "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
+  scores: [
+    "4",
+    "2",
+    "5",
+    "1",
+    "3",
+    "2",
+    "2",
+    "1",
+    "3",
+    "2"
+  ]
+  },
+  {
+  name: "Jeremiah Scanlon",
+  photo: "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
+  scores: [
+    "5",
+    "2",
+    "2",
+    "2",
+    "4",
+    "1",
+    "3",
+    "2",
+    "5",
+    "5"
+  ]
+  },
+  {
+  name: "Louis T. Delia",
+  photo: "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
+  scores: [
+    "3",
+    "3",
+    "4",
+    "2",
+    "2",
+    "1",
+    "3",
+    "2",
+    "2",
+    "3"
+  ]
+  },
+  {
+  name: "Lou Ritter",
+  photo: "https://m.facebook.com/photo.php?fbid=10208500699025296&id=1542229019&set=a.1549418665704.77596.1542229019&source=11",
+  scores: [
+    "4",
+    "3",
+    "4",
+    "1",
+    "5",
+    "2",
+    "5",
+    "3",
+    "1",
+    "4"
+  ]
+  },
+  {
+  name: "Jordan Biason",
+  photo: "https://scontent-ord1-1.xx.fbcdn.net/v/t1.0-9/12741971_10205764267089153_4212986785721953092_n.jpg?oh=4e18265f7d380167223a97fbd7eba278&oe=57B78445",
+  scores: [
+    "4",
+    "4",
+    "2",
+    "3",
+    "2",
+    "2",
+    "3",
+    "2",
+    "4",
+    "5"
+  ]
+  },
+  {
+  name: "keisha",
+  photo: "http://r.ddmcdn.com/s_f/o_1/cx_0/cy_157/cw_1327/ch_1327/w_720/APL/uploads/2013/01/smart-cat-article.jpg",
+  scores: [
+    "2",
+    "3",
+    "3",
+    "3",
+    "2",
+    "3",
+    "3",
+    "3",
+    "2",
+    "4"
+  ]
+  },
+  {
+  name: "George Jetson",
+  photo: "http://allthingsd.com/files/2013/10/georgejetson380.jpg",
+  scores: [
+    "4",
+    "1",
+    "3",
+    "3",
+    "4",
+    "3",
+    "5",
+    "1",
+    "2",
+    "4"
+  ]
+  },
+  {
+  name: "George Jetson",
+  photo: "http://allthingsd.com/files/2013/10/georgejetson380.jpg",
+  scores: [
+    "4",
+    "1",
+    "3",
+    "3",
+    "4",
+    "3",
+    "5",
+    "1",
+    "2",
+    "4"
+  ]
+  },
+  {
+  name: "ruphus",
+  photo: "http://noclipmode.com/wp-content/uploads/2010/11/A-pimp-named-Slickback.png",
+  scores: [
+    "1",
+    "3",
+    "1",
+    "4",
+    "1",
+    "5",
+    "3",
+    "5",
+    "3",
+    "3"
+  ]
+  },
+  {
+  name: "Milo",
+  photo: "https://www.what-dog.net/Images/faces2/scroll0015.jpg",
+  scores: [
+    "2",
+    "3",
+    "3",
+    "4",
+    "3",
+    "2",
+    "2",
+    "4",
+    "3",
+    "3"
+  ]
+  },
+  {
+  name: "tom",
+  photo: "bomb",
+  scores: [
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2",
+    "2"
+  ]
+  },
+  {
+  name: "me",
+  photo: "https://www.pexels.com/photo/man-young-happy-smiling-91227/",
+  scores: [
+    "3",
+    "2",
+    "2",
+    "1",
+    "1",
+    "1",
+    "1",
+    "2",
+    "5",
+    "2"
+  ]
+  },
+  {
+  name: "test",
+  photo: "test",
+  scores: [
+    "2",
+    "4",
+    "3",
+    "2",
+    "5",
+    "4",
+    "4",
+    "1",
+    "3",
+    "1"
+  ]
+  },
+  {
+  name: "Panko ",
+  photo: "www.google.com",
+  scores: [
+    "1",
+    "1",
+    "1",
+    "5",
+    "1",
+    "5",
+    "2",
+    "5",
+    "3",
+    "1"
+  ]
+  },
+  {
+  name: "b",
+  photo: "a",
+  scores: [
+    "1",
+    "3",
+    "4",
+    "4",
+    "3",
+    "2",
+    "1",
+    "2",
+    "3",
+    "1"
+  ]
+  },
+  {
+  name: "b",
+  photo: "a",
+  scores: [
+    "1",
+    "3",
+    "4",
+    "4",
+    "3",
+    "2",
+    "1",
+    "2",
+    "3",
+    "1"
+  ]
+  },
+  {
+  name: "Munchie",
+  photo: "http://i1.mirror.co.uk/incoming/article7731571.ece/ALTERNATES/s298/Pokemon-charmander.png",
+  scores: [
+    "1",
+    "3",
+    "3",
+    "1",
+    "2",
+    "2",
+    "4",
+    "1",
+    "2",
+    "1"
+  ]
+  },
+  {
+  name: "Munchie",
+  photo: "http://i1.mirror.co.uk/incoming/article7731571.ece/ALTERNATES/s298/Pokemon-charmander.png",
+  scores: [
+    "1",
+    "3",
+    "3",
+    "1",
+    "2",
+    "2",
+    "4",
+    "1",
+    "2",
+    "1"
+  ]
+  },
+  {
+  name: "Munchie",
+  photo: "http://i1.mirror.co.uk/incoming/article7731571.ece/ALTERNATES/s298/Pokemon-charmander.png",
+  scores: [
+    "1",
+    "3",
+    "3",
+    "1",
+    "2",
+    "2",
+    "4",
+    "1",
+    "2",
+    "1"
+  ]
+  },
+  {
+  name: "test",
+  photo: "http://lorempixel.com/output/food-q-c-400-400-4.jpg",
+  scores: [
+    "1",
+    "2",
+    "3",
+    "3",
+    "5",
+    "4",
+    "1",
+    "1",
+    "3",
+    "1"
+  ]
+  },
+  {
+  name: "b",
+  photo: "a",
+  scores: [
+    "1",
+    "3",
+    "4",
+    "4",
+    "3",
+    "2",
+    "1",
+    "2",
+    "3",
+    "1"
+  ]
+  },
+  {
+  name: "aa",
+  photo: "n b",
+  scores: [
+    "3",
+    "3",
+    "5",
+    "1",
+    "2",
+    "3",
+    "5",
+    "1",
+    "2",
+    "4"
+  ]
+  },
+  {
+  name: "Adam",
+  photo: "google.com",
+  scores: [
+    "5",
+    "5",
+    "5",
+    "5",
+    "5",
+    "5",
+    "5",
+    "5",
+    "5",
+    "5"
+  ]
+  },
+  {
+  name: "ay",
+  photo: ".img",
+  scores: [
+    "1",
+    "3",
+    "3",
+    "2",
+    "4",
+    "4",
+    "3",
+    "4",
+    "2",
+    "2"
+  ]
+  },
+  {
+  name: "b",
+  photo: "a",
+  scores: [
+    "1",
+    "3",
+    "4",
+    "4",
+    "3",
+    "2",
+    "1",
+    "2",
+    "3",
+    "1"
+  ]
+  },
+  {
+  name: "Zeus",
+  photo: "https://upload.wikimedia.org/wikipedia/commons/c/c8/Jupiter_Smyrna_Louvre_Ma13.jpg",
+  scores: [
+    "5",
+    "4",
+    "5",
+    "3",
+    "2",
+    "4",
+    "5",
+    "4",
+    "2",
+    "5"
+  ]
+  },
+  {
+  name: "BananaBoat",
+  photo: "http://i.imgur.com/Nmsfxm8.jpg",
+  scores: [
+    "3",
+    "2",
+    "4",
+    "2",
+    "4",
+    "2",
+    "3",
+    "4",
+    "4",
+    "3"
+  ]
+  }
+];
+
+module.exports = friends;
